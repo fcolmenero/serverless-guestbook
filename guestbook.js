df4df1a37e7007731cf98632e2a2f7420b120a21
Javier Colmenero <fcolmenero78@gmail.com>
2022-05-01 13:18:01 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://7051d931.eu-gb.apigw.appdomain.cloud/guestbook';
+const apiUrl = '';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
